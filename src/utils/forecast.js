3b4366b939d34f81e7ce067ecb99a72350d013ae
Alexies De Guzman <alexies.de.guzman@accenture.com>
2021-03-01 16:10:44 +0800

    Add precip and humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,11 +5,11 @@ const forecast = (latitude, longitude,callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=6f161702ff871393c3d20e298f89b06c&query='+ latitude +','+ longitude +'&units=m'; 
     request({url: url, json: true},(error, {body})=>{
         if(error){
-            callback(chalk.red.inverse('Unable to connect to weather service'), undefined);
+            callback('Unable to connect to weather service', undefined);
         }else if(body.error){
-            callback(chalk.red.inverse('Unable to find location'), undefined);
+            callback('Unable to find location', undefined);
         }else{
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The chance of rain is probably '+ body.current.precip + '% and the humidity is '+ body.current.humidity + '%.');
         }
     })
 }
